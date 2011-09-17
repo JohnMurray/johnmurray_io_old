83ecbd6342812219334247fe24d8cc3940feeaf7
John Murray <john.m.murray786@gmail.com>
2011-09-17 17:20:13 -0400

    updated basic routes in sinatra

--- sinatra-app.rb
+++ sinatra-app.rb
@@ -1,4 +1,20 @@
+require 'haml'
 require 'sinatra'
 require './config/sinatra-config.rb'
 
+get '/' do 
+  haml :index
+end
 
+get '/contact' do
+  @body = :contact
+  haml :template
+end
+
+get '/log' do
+  @body = :log
+  haml :template
+end
+
+get '/log/:year/:month/:day/:title' do
+end
