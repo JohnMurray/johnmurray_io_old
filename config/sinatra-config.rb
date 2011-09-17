54b8bf1df497f3da65d9df8c669eec5b79ddb848
John Murray <john.m.murray786@gmail.com>
2011-09-17 17:11:32 -0400

    removed db-config from config files

--- config/sinatra-config.rb
+++ config/sinatra-config.rb
@@ -36,22 +36,3 @@ end
 
 
 
-##----
-## DB/ACTIVE RECORD SETTINGS
-##   Configuration and connection settings. The first section configures the local
-##   environment using sqlite and logging. The second (production) sections assumes
-##   that we are running on Heroku and so we are using the provided Postgres SQL DB
-##   provided.
-##----
-$: << File.join(settings.root, 'lib')
-require 'heroku-helper'
-
-if [:development, :test].include? settings.environment.to_sym then
-  db_config = YAML::load File.open(File.join(settings.root, 'db', 'config', 'db.yml'))
-  db_env = settings.environment
-  db_config[db_env][:database] = File.join settings.root, 'db', 'sqlite', db_config[db_env][:database]
-  ActiveRecord::Base.establish_connection(db_config[db_env])
-  ActiveRecord::Base.logger = Logger.new(File.open(File.join(settings.logdir, 'database.log'), 'a'))
-elsif HerokuHelper.in_heroku?
-  ActiveRecord::Base.establish_connection(HerokuHelper.get_db_env)
-end
