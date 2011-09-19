f5ebec7ae722336b78d74c2ddcd5ada8bcdb6fe9
John Murray <john.m.murray786@gmail.com>
2011-09-19 07:05:03 -0400

    updated styles and code for blog parsing

--- sinatra-app.rb
+++ sinatra-app.rb
@@ -1,4 +1,5 @@
 require 'haml'
+require 'less'
 require 'sinatra'
 require './config/sinatra-config.rb'
 
@@ -7,14 +8,31 @@ get '/' do
 end
 
 get '/contact' do
-  @body = :contact
-  haml :template
+  haml :contact
 end
 
 get '/log' do
-  @body = :log
-  haml :template
+  @files = []
+  regex = /blogs\/(?<year>\d+)\/(?<month>\d+)\/(?<day>\d+)\/(?<title>.*\.md)/
+  Dir[File.join("blogs", "**", "*.md")].each do |file|
+    match_data = regex.match file
+    @files << {
+      path:  "/log/#{match_data[:year]}/#{match_data[:month]}/#{match_data[:day]}/#{match_data[:title]}",
+      name:  match_data[:title].gsub(/([^-])-([^-])/, '\1 \2').gsub('--', '-').gsub('.md', ''),
+      date:  Time.new(match_data[:year], match_data[:month], match_data[:day])
+    }
+  end
+  #haml :log
+  @files.inspect.to_s
 end
 
 get '/log/:year/:month/:day/:title' do
+end
+
+
+##----
+## Server LESS CSS files as css
+##----
+get '/stylesheet.css' do
+  less :stylesheet
 end
