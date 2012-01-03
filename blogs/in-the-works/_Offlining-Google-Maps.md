6aa3ee82a32ddf665c889f49521f9cc15839a83f
John Murray <john.m.murray786@gmail.com>
2012-01-03 08:26:49 -0500

    Added new (in-the-works) log entry and a viewer
    Needed a way to see log entries before they are published (without
    always running it locally). So, I have created a path that we can hit
    from /log/beta to get a list of un-published entries for review.

---
+++ blogs/in-the-works/_Offlining-Google-Maps.md
@@ -0,0 +1,5 @@
+##----
+## Make this more of a tutorial than anything but, with some upbeat language. 
+## Need to figure out how tile are loaded (which would make me look a lot
+## smarter and the tut much meatier).
+##----
--- sinatra-app.rb
+++ sinatra-app.rb
@@ -75,6 +75,47 @@ get '/log/:year/:month/:day/:title' do
   haml :log_entry
 end
 
+##----
+## A directory of all 'log entries' that are currently in progress. This will
+## not be visible from the main site. It is solely for the purpose of demo'ing
+## my log entries to myself to ensure proper formatting.
+##----
+get '/log/beta' do
+  @files = []
+  regex = /blogs\/in-the-works\/(?<title>.*\.md)/
+  Dir[File.join('blogs', 'in-the-works', '_*.md')].each do |file|
+    match = regex.match file
+    @files << {
+      path:  "/log/beta/#{match[:title]}",
+      name:  match[:title]
+               .gsub(/([^-])-([^-])/, '\1 \1')
+               .gsub('--', '-')
+               .gsub('.md', ''),
+      date:  Time.now
+    }
+  end
+  haml :log
+end
+
+##----
+## Load a specific 'log entry' that is in the works for review. As the normal
+## log entries, this is just a markdown file.
+##----
+get '/log/beta/:title' do
+  @js_s = PrettifierHelper.get_scripts ['ruby', 'scala', 'css', 'yaml']
+  markdown_file = File.open(File.join(
+    'blogs',
+    'in-the-works',
+    params[:title])).read
+  doc = Maruku.new(markdown_file)
+  @doc = doc.to_html
+  @doc_title = params[:title]
+                 .gsub(/([^-])-([^-])/, '\1 \2')
+                 .gsub('--', '-')
+                 .gsub('.md', '')
+  haml :log_entry
+end
+
 
 
 ##----
