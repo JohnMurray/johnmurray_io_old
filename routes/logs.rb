d94931ae04f17671a5211852074750bc52955027
John Murray <me@johnmurray.io>
2014-02-08 22:32:12 -0500

    Related Articles - Rough Draft

--- routes/logs.rb
+++ routes/logs.rb
@@ -2,6 +2,16 @@ $: << ::File.expand_path('../../lib', __FILE__)
 require 'log-helper'
 
 class JMApp < Sinatra::Base
+  LOG_META = {
+    regex: /blogs\/(?<year>\d+)\/(?<month>\d+)\/(?<day>\d+)\/(?<title>.*\.md)/,
+    path:  File.join("blogs", "**", "[^_]*.md")
+  }
+
+  LOG_PRE_META = {
+    regex: /blogs\/in-the-works\/(?<year>\d+)\/(?<month>\d+)\/(?<day>\d+)\/(?<title>.*\.md)/,
+    path:  File.join('blogs', 'in-the-works', '**', '_*.md')
+  }
+
   ##----
   ## A directory of all 'log entries' that are currently in progress. This will
   ## not be visible from the main site. It is solely for the purpose of demo'ing
@@ -10,11 +20,11 @@ class JMApp < Sinatra::Base
   get '/log/pre' do
     with_cache('log-pre') do
 
-      regex = /blogs\/in-the-works\/(?<year>\d+)\/(?<month>\d+)\/(?<day>\d+)\/(?<title>.*\.md)/
-      path = File.join('blogs', 'in-the-works', '**', '_*.md')
       pre = true
 
-      @files = with_cache('log-pre-file-list') { log_list(path, regex, pre) }
+      @files = with_cache('log-pre-file-list') { 
+        log_list(LOG_PRE_META[:path], LOG_PRE_META[:regex], pre) 
+      }
 
       haml :log
     end
@@ -57,9 +67,9 @@ class JMApp < Sinatra::Base
   get '/log' do
     with_cache('log') do
 
-      regex = /blogs\/(?<year>\d+)\/(?<month>\d+)\/(?<day>\d+)\/(?<title>.*\.md)/
-      path = File.join("blogs", "**", "[^_]*.md")
-      @files = with_cache('log-file-list') { log_list(path, regex) }
+      @files = with_cache('log-file-list') { 
+        log_list(LOG_META[:path], LOG_META[:regex]) 
+      }
 
       haml(:log)
     end
@@ -72,7 +82,7 @@ class JMApp < Sinatra::Base
   ## that we are parsing and loading into a page.
   ##----
   get '/log/:year/:month/:day/*' do
-    with_cache(request.path_info) do
+    #with_cache(request.path_info) do
       title = params[:splat].join('')
 
       file_name = File.join(
@@ -88,7 +98,61 @@ class JMApp < Sinatra::Base
 
       @doc = parse_file(file_name)
       @doc_title = format_title(title)
+
+      @related_logs = related_logs(@doc_title)
+      STDERR.puts @related_logs.map{|l| l[:name]}.join("\n")
+
       haml :log_entry
+    #end
+  end
+
+
+
+  ##----
+  ## Public: Build a list of 'file' objects (same as used when showing
+  ## the log-list(s)) and return the items that are related to the current
+  ## post (specified by the 'title' parameter).
+  ## 
+  ## Params:
+  ##   title  Title of article to compare with file-list to find current.
+  ##   pre    Boolean indicating whether this is a finished or work-in-progress
+  ##          (pre) log entry. Determines different file-list to process.
+  ## 
+  ## Returns an array of hashes with a pre-determined maximum length
+  ## 
+  ##----
+  def related_logs(title, pre=false)
+
+    logs = begin
+      if pre
+        with_cache('log-pre-file-list') {
+          log_list(LOG_PRE_META[:path], LOG_PRE_META[:regex], pre)
+        }
+      else
+        with_cache('log-file-list') {
+          log_list(LOG_META[:path], LOG_META[:regex])
+        }
+      end
     end
+
+    related_files = []
+    curr_file = logs.select{|l| l[:name] == title}.first
+    logs.delete(curr_file)
+    top_logs = logs.select{|l| l[:date] > curr_file[:date]}.reverse
+
+    # todo: handle use-case of now current file
+    if curr_file
+      top_logs.each do |l|
+          related_files << l if related_files.count < 3 && title != l[:name]
+      end
+    else
+      related_files = logs.take(3)
+    end
+
+    if related_files.count < 3
+      related_files.concat(logs.take(3 - related_files.count)).uniq!
+    end
+
+    related_files
   end
 end
